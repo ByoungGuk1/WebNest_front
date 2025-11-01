2f08194bfbaeb0dce4c45eff6d82ee53dfbd0cf8
ByoungGuk1 <bksong121212@gmail.com>
2025-11-02 00:16:16 +0900

    로그인 페이지 스타일 추가
    add public/assets/font/Romanticgumi/ : 로고용 폰트 추가
    add public/assets/icons/loginIcon.png : 로그인 버튼 아이콘 이미지 추가
    src/styles/global.js : 추가한 폰트 사용을 위한 폰트패밀리 추가

--- src/pages/user/signin/SignInContainer.jsx
+++ src/pages/user/signin/SignInContainer.jsx
@@ -1,12 +1,60 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import S from "./style";
-
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 const SignInContainer = () => {
+  let isEyeOpen = false;
   return (
     <div>
       <S.ContentContainer>
-        <div>Sign In</div>
+        <S.LogoWrapper>
+          <S.logo_grean>Web</S.logo_grean>
+          <S.logo_blue>Nest</S.logo_blue>
+        </S.LogoWrapper>
+
+        <S.LoginForm>
+          <S.InputWrapper>
+            <S.Input placeholder="아이디를 입력하세요" />
+          </S.InputWrapper>
+          <S.InputWrapper>
+            <S.Input placeholder="비밀번호를 입력하세요" />
+            <S.passwordEye>
+              <FontAwesomeIcon
+                icon={isEyeOpen ? faEye : faEyeSlash}
+                size="lg"
+              />
+            </S.passwordEye>
+          </S.InputWrapper>
+          <S.CheckBoxLabel>
+            <S.CheckBox type="checkbox" />
+            로그인 상태 유지
+          </S.CheckBoxLabel>
+          <S.Button>
+            <img src="/assets/icons/loginIcon.png" />
+            <span>로그인</span>
+          </S.Button>
+        </S.LoginForm>
+
+        <S.UserPageLINK>
+          <S.UserLink to="/sign-up">회원가입</S.UserLink>
+          <S.UserLink to="/find-id">아이디 찾기</S.UserLink>
+          <S.UserLink to="/find-password">비밀번호 찾기</S.UserLink>
+        </S.UserPageLINK>
+
+        <S.OAuthLinkContainer>
+          <p>sns 계정으로 간편하게 시작하기</p>
+          <S.OAuthLinkWrapper>
+            <S.OAuthLink to="/sign-up">
+              <img src="/assets/icons/googleicon.png" />
+            </S.OAuthLink>
+            <S.OAuthLink to="/sign-up">
+              <img src="/assets/icons/kakaoicon.png" />
+            </S.OAuthLink>
+            <S.OAuthLink to="/sign-up">
+              <img src="/assets/icons/navericon.png" />
+            </S.OAuthLink>
+          </S.OAuthLinkWrapper>
+        </S.OAuthLinkContainer>
       </S.ContentContainer>
     </div>
   );
--- src/pages/user/signin/style.js
+++ src/pages/user/signin/style.js
@@ -1,13 +1,139 @@
 import styled from "styled-components";
-import { flexCenter } from "../../../styles/common";
+import {
+  flexBeetweenColumn,
+  flexBeetweenRow,
+  flexCenter,
+  h4Medium,
+  h5Bold,
+  h6Medium,
+  h7Medium,
+} from "../../../styles/common";
+import { Link } from "react-router-dom";
 
 const S = {};
 
 S.ContentContainer = styled.div`
   ${flexCenter};
+  flex-direction: column;
   width: 615px;
   height: 610px;
-  margin: 0 auto;
+  margin: 160px auto 0 auto;
+  background-color: ${({ theme }) => theme.PALETTE.neutral.white.main};
+`;
+
+S.LogoWrapper = styled.div`
+  ${flexCenter};
+  margin: 48px auto 42px auto;
+`;
+
+S.logo_grean = styled.h4`
+  font-family: "RomanticGumi";
+  text-decoration: none;
+  color: ${({ theme }) => theme.PALETTE.primary.green.main};
+  ${h4Medium}
+`;
+
+S.logo_blue = styled.h4`
+  font-family: "RomanticGumi";
+  text-decoration: none;
+  color: ${({ theme }) => theme.PALETTE.primary.blue.main};
+  ${h4Medium}
+`;
+
+S.LoginForm = styled.div`
+  ${flexBeetweenColumn};
+  height: 270px;
+`;
+
+S.InputWrapper = styled.div`
+  ${flexCenter};
+  width: 520px;
+  height: 52px;
+  border: 1px solid ${({ theme }) => theme.PALETTE.neutral.black.disable};
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.PALETTE.neutral.white.secondary};
+`;
+
+S.Input = styled.input`
+  width: 100%;
+  border: none;
+  padding: 0 20px;
+  font-size: 16px;
+  ${h6Medium}
+  background-color: ${({ theme }) => theme.PALETTE.neutral.white.secondary};
+  &::placeholder {
+    color: ${({ theme }) => theme.PALETTE.neutral.black.disable};
+  }
+  &:focus {
+    outline: none;
+  }
+`;
+
+S.passwordEye = styled.button`
+  ${flexCenter};
+  margin-right: 20px;
+  background-color: transparent;
+`;
+
+S.CheckBoxLabel = styled.label`
+  ${flexBeetweenRow};
+  justify-content: start;
+  gap: 8px;
+  font-size: 14px;
+  width: 100%;
+  color: ${({ theme }) => theme.PALETTE.neutral.black.secondary};
+  ${h7Medium}
+`;
+
+S.CheckBox = styled.input`
+  width: 20px;
+  height: 20px;
+`;
+
+S.Button = styled.button`
+  ${flexCenter};
+  gap: 16px;
+  width: 520px;
+  height: 58px;
+  background-color: ${({ theme }) => theme.PALETTE.primary.purple.main};
+  color: ${({ theme }) => theme.PALETTE.neutral.white.main};
+  ${h5Bold}
+  border: none;
+  border-radius: 8px;
+`;
+
+S.UserPageLINK = styled.div`
+  ${flexBeetweenRow};
+  gap: 32px;
+  margin: 22px auto 0 auto;
+`;
+
+S.UserLink = styled(Link)`
+  min-width: 72px;
+  ${flexCenter}
+  font-size: 14px;
+  color: ${({ theme }) => theme.PALETTE.neutral.black.secondary};
+  text-decoration: none;
+  ${h7Medium}
+`;
+
+S.OAuthLinkContainer = styled.div`
+  margin: 30px auto auto auto;
+  ${flexCenter};
+  flex-direction: column;
+  ${h7Medium}
+  color: ${({ theme }) => theme.PALETTE.neutral.black.secondary};
+`;
+
+S.OAuthLinkWrapper = styled.div`
+  ${flexBeetweenRow};
+  gap: 24px;
+  margin: 12px auto 0 auto;
+`;
+
+S.OAuthLink = styled(Link)`
+  width: 50px;
+  height: 50px;
 `;
 
 export default S;
