7f8769be0bbb38aa84e36c12859deda9876688e1
ByoungGuk1 <bksong121212@gmail.com>
2025-11-01 20:48:20 +0900

    11

---
+++ src/pages/user/signin/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { flexCenter } from "../../../styles/common";
+
+const S = {};
+
+S.ContentContainer = styled.div`
+  ${flexCenter};
+  width: 615px;
+  height: 610px;
+  margin: 0 auto;
+`;
+
+export default S;
