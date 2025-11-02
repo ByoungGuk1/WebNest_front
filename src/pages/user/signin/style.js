bc22955e79332b5e1e53350a7ade444a7c58e52f
ByoungGuk1 <bksong121212@gmail.com>
2025-11-02 10:48:13 +0900

    아이디 및 비밀번호 찾기 페이지 스타일 추가
    src/pages/user/findid/ : 아이디 찾기 페이지 style 추가
    src/pages/user/findpassword/ : 비밀번호 찾기 페이지 style 추가
    src/pages/user/signin : user 전역 스타일 파일 생성으로 인한 스타일 값 조정
    src/pages/user/style.js : user 전역 스타일 파일 생성 ( Su 객체)

--- src/pages/user/signin/style.js
+++ src/pages/user/signin/style.js
@@ -3,97 +3,15 @@ import {
   flexBeetweenColumn,
   flexBeetweenRow,
   flexCenter,
-  h4Medium,
-  h5Bold,
-  h6Medium,
   h7Medium,
 } from "../../../styles/common";
 import { Link } from "react-router-dom";
 
 const S = {};
 
-S.ContentContainer = styled.div`
-  ${flexCenter};
-  flex-direction: column;
-  width: 615px;
-  height: 610px;
-  margin: 160px auto 0 auto;
-  background-color: ${({ theme }) => theme.PALETTE.neutral.white.main};
-`;
-
-S.LogoWrapper = styled.div`
-  ${flexCenter};
-  margin: 48px auto 42px auto;
-`;
-
-S.logo_grean = styled.h4`
-  font-family: "RomanticGumi";
-  text-decoration: none;
-  color: ${({ theme }) => theme.PALETTE.primary.green.main};
-  ${h4Medium}
-`;
-
-S.logo_blue = styled.h4`
-  font-family: "RomanticGumi";
-  text-decoration: none;
-  color: ${({ theme }) => theme.PALETTE.primary.blue.main};
-  ${h4Medium}
-`;
-
 S.LoginForm = styled.form`
   ${flexBeetweenColumn};
   height: 270px;
-`;
-
-S.InputWrapper = styled.div`
-  ${flexCenter};
-  width: 520px;
-  height: 52px;
-  border: 1px solid ${({ theme }) => theme.PALETTE.neutral.black.disable};
-  border-radius: 8px;
-  background-color: ${({ theme }) => theme.PALETTE.neutral.white.secondary};
-`;
-
-S.Input = styled.input`
-  width: 100%;
-  border: none;
-  padding: 0 20px;
-  font-size: 16px;
-  ${h6Medium}
-  background-color: ${({ theme }) => theme.PALETTE.neutral.white.secondary};
-  &::placeholder {
-    color: ${({ theme }) => theme.PALETTE.neutral.black.disable};
-  }
-  &:focus {
-    outline: none;
-  }
-`;
-
-S.CheckBoxLabel = styled.label`
-  ${flexBeetweenRow};
-  justify-content: start;
-  gap: 8px;
-  font-size: 14px;
-  width: 100%;
-  color: ${({ theme }) => theme.PALETTE.neutral.black.secondary};
-  ${h7Medium}
-`;
-
-S.CheckBox = styled.input`
-  width: 20px;
-  height: 20px;
-`;
-
-S.Button = styled.button`
-  ${flexCenter};
-  gap: 16px;
-  width: 520px;
-  height: 58px;
-  background-color: ${({ theme }) => theme.PALETTE.primary.purple.main};
-  color: ${({ theme }) => theme.PALETTE.neutral.white.main};
-  ${h5Bold}
-  border: none;
-  border-radius: 8px;
 `;
 
 S.UserPageLINK = styled.div`
@@ -112,7 +30,7 @@ S.UserLink = styled(Link)`
 `;
 
 S.OAuthLinkContainer = styled.div`
-  margin: 30px auto auto auto;
+  margin: 30px auto 0 auto;
   ${flexCenter};
   flex-direction: column;
   ${h7Medium}
