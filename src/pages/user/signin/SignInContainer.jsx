d741e2dc460cb53529d0ccfb4eaf63d1b253eb43
ByoungGuk1 <bksong121212@gmail.com>
2025-11-07 14:24:24 +0900

    로컬 회원가입 및 로그인 추가

--- src/pages/user/signin/SignInContainer.jsx
+++ src/pages/user/signin/SignInContainer.jsx
@@ -14,17 +14,20 @@ const SignInContainer = () => {
   const { isLogin } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const [isEyeOpen, setIsEyeOpen] = useState(false);
+  const [showEmailSend, setShowEmailSend] = useState(false);
+  const [showEmailVerify, setShowEmailVerify] = useState(false);
 
   const {
     register,
     handleSubmit,
+    trigger,
     getValues,
     formState: { isSubmitting, isSubmitted, errors },
   } = useForm({ mode: "onChange" });
 
   const handleSumbmitForm = handleSubmit(async (data) => {
     const { ...member } = data;
-
+    console.log(data);
     await fetch(`${process.env.REACT_APP_BACKEND_URL}/auth/login`, {
       headers: {
         "Content-Type": "application/json",
@@ -45,6 +48,10 @@ const SignInContainer = () => {
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[!@#])[\da-zA-Z!@#]{8,}$/;
 
+  const handleEmailBlur = async (e) => {
+    await trigger("email");
+  };
+
   return (
     <div>
       <Su.ContentContainer>
@@ -57,22 +64,27 @@ const SignInContainer = () => {
           <Su.InputWrapper>
             <Su.Input
               type="text"
-              placeholder="아이디를 입력하세요"
-              name="memberEmail"
-              {...register("memberEmail", {
-                required: true,
+              placeholder="이메일"
+              readOnly={showEmailSend || showEmailVerify}
+              {...register("userEmail", {
+                required: "이메일을 입력해주세요.",
                 pattern: {
                   value: emailRegex,
+                  message: "이메일 형식이 올바르지 않습니다.",
                 },
               })}
+              onBlur={handleEmailBlur}
             />
           </Su.InputWrapper>
+          {errors.userEmail && (
+            <Su.AlertText>{errors.userEmail.message}</Su.AlertText>
+          )}
           <Su.InputWrapper>
             <Su.Input
               type={isEyeOpen ? "text" : "password"}
               placeholder="비밀번호를 입력하세요"
-              name="memberPassword"
-              {...register("memberPassword", {
+              name="userPassword"
+              {...register("userPassword", {
                 required: true,
                 pattern: {
                   value: passwordRegex,
