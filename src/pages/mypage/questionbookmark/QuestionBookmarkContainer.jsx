098069d4c47b216197ef2085c61a5155244927ec
DESKTOP-8EQJQIE\user <bksong121212@gmail.com>
2025-11-14 17:39:30 +0900

    중간 커밋

--- src/pages/mypage/questionbookmark/QuestionBookmarkContainer.jsx
+++ src/pages/mypage/questionbookmark/QuestionBookmarkContainer.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import S from "../../quiz/style"
-import QiuzDropDown from 'pages/quiz/quizdropdown/QiuzDropDown';
-import QuizPage from 'pages/quiz/quizpage/QuizPage';
 
 const QuestionBookmarkContainer = () => {
   return (
@@ -9,9 +7,7 @@ const QuestionBookmarkContainer = () => {
       <S.AllContainer>
         <S.InnerContainer>
           {/* 드롭다운버튼 */}
-          <QiuzDropDown />
           {/* 문제 리스트*/}
-          <QuizPage />
         </S.InnerContainer>
       </S.AllContainer>
     </div>
